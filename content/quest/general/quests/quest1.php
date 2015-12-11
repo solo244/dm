5fbc53e90d8b522d28267c0e932f7a6a736d1d72
Solo244 <solo244@gmail.com>
2015-12-11 16:06:45 +0100

    Quest
    Fix for multi text

--- content/quest/general/quests/quest1.php
+++ content/quest/general/quests/quest1.php
@@ -13,37 +13,44 @@
 
 <p><i class="fa fa-commenting" style="color: white"></i>
 
-<!-- Description-->		Go find a weapon in a nearby cave. This weapon is an ancient
-											heirloom and has emotional meaning. It was stolen at night by
-											goblins. If you bring it back there is gold reward in it for
-											you.
+<!-- Description-->		
+Go find a weapon in a nearby cave. This weapon is an ancient
+heirloom and has emotional meaning. It was stolen at night by
+goblins. If you bring it back there is gold reward in it for
+you.
 
 </p><p><i class="fa fa-map" style="color: white"></i>
 
-<!-- Location -->			Goblin cave
+<!-- Location -->			
+Goblin cave
 
 </p><p><i class="fa fa-crosshairs" style="color: white"></i>
 
-<!-- Enemies -->			Goblin grunt, goblin militairy and goblin shaman
+<!-- Enemies -->			
+Goblin grunt, goblin militairy and goblin shaman
 
 </p><p><i class="fa fa-check" style="color: green"></i>
 
-<!-- Completed -->		The goblins have a secret treasure room that contains not only
-											the weapon, but also the gold and stuff the goblins have stolen
-											(gp and potions). You can take it by overpowering or killing them.
+<!-- Completed -->		
+The goblins have a secret treasure room that contains not only
+the weapon, but also the gold and stuff the goblins have stolen
+(gp and potions). You can take it by overpowering or killing them.
 
 </p><p><i class="fa fa-times" style="color: red"></i>
 
-<!-- Failed -->				Killed all the goblins before they can tell you where the
-											gold/treasure is.
+<!-- Failed -->				
+Killed all the goblins before they can tell you where the
+gold/treasure is.
 
 </p><p><i class="fa fa-reply-all" style="color: white"></i>
 
-<!-- Surprise -->			The weapon makes the wielder talk to dead people and Dagond wants
-											it to keep communicating to his wife Draneny
+<!-- Surprise -->			
+The weapon makes the wielder talk to dead people and Dagond wants
+it to keep communicating to his wife Draneny
 
 </p><p><i class="fa fa-exchange" style="color: white"></i>
 
-<!-- Alts -->					Eleanor can contact his wife and let them say goodbye.
+<!-- Alts -->					
+Eleanor can contact his wife and let them say goodbye.
 
 <?php include '/../template/bottom.php' ?>
