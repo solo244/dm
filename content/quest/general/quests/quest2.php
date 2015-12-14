b27269f61ece13e0074485bcbb584b5ed0b9fdb0
Solo244 <solo244@gmail.com>
2015-12-14 16:53:50 +0100

    Quests
    Added a general/quest2

---
+++ content/quest/general/quests/quest2.php
@@ -0,0 +1,51 @@
+<?php include '/../template/top.php' ?>
+
+<!-- Carousel images -->
+
+<img src="images/quests/general/q1a.jpg" class="toResizeClass img1">
+<img src="images/quests/general/q1b.jpg" class="toResizeClass img2">
+
+<?php include '/../template/middle.php' ?>
+
+<!-- The Walking Dead -->
+
+<!-- Happy ending -->
+
+<p><i class="fa fa-commenting" style="color: white"></i>
+
+<!-- Description-->		
+There have long been rumours of the cemetery being desecrated 
+by someone stealing the corpses. One night the PCs chance upon 
+the necromancer walking home with newly animated undead.
+
+</p><p><i class="fa fa-map" style="color: white"></i>
+
+<!-- Location -->			
+The cemetery
+
+</p><p><i class="fa fa-crosshairs" style="color: white"></i>
+
+<!-- Enemies -->			
+Necromancer and about 10 undead (family members) & Guards
+
+</p><p><i class="fa fa-check" style="color: green"></i>
+
+<!-- Completed -->		
+Kill the necromancer
+
+</p><p><i class="fa fa-times" style="color: red"></i>
+
+<!-- Failed -->				
+Killing the necromancer in front of the guards. Not killing the guards
+
+</p><p><i class="fa fa-reply-all" style="color: white"></i>
+
+<!-- Surprise -->			
+The necromancer payed the major for these corpses and his guards will also attack you when they see you attacking the necromancer.
+
+</p><p><i class="fa fa-exchange" style="color: white"></i>
+
+<!-- Alts -->					
+Pay off the guards and let them help you.
+
+<?php include '/../template/bottom.php' ?>
