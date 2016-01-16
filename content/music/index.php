6a4694e4f0056d9efdab8c4c41240ef17d90d962
Solo244 <solo244@gmail.com>
2016-01-16 23:00:37 +0100

    Sound
    Added icons

--- content/music/index.php
+++ content/music/index.php
@@ -1,3 +1,19 @@
 <div class="dashHeader">
   <h2>Music</h2>
+
+  <i class="fa fa-soundcloud" id="soundMood"></i>
+  <!-- -->
+  <i class="fa fa-chain-broken" id="soundBreaking"></i>
+  <i class="fa fa-space-shuttle" id="soundCombat"></i>
+  <i class="fa fa-star" id="soundEnvironment"></i>
+  <i class="fa fa-bug" id="soundMonsters"></i>
+  <i class="fa fa-tree" id="soundNature"></i>
+  <!-- -->
+</div>
+
+<div class="innerContent" id="allSounds">
+  <div class="">
+    <i class="fa fa-arrow-left backNPC"></i>
+    <div class="" id=""><?php include 'breaking\index.php' ?></div>
+  </div>
 </div>
