1cd4944ce23bab808b26ab4334d867547140814f
Solo244 <solo244@gmail.com>
2016-01-25 17:01:45 +0100

    Combat

--- content/combat/index.php
+++ content/combat/index.php
@@ -1,35 +1,20 @@
 <div class="dashHeader">
   <h2>Combat</h2>
 
-  <i class="fa fa-magic" id="combatSort"></i>
-  <i class="fa fa-user" id="combatSave"></i>
-  <i class="fa fa-building" id="combatLoad"></i>
-  <i class="fa fa-building" id="combatPause"></i>
+  <i class="fa fa-refresh" id="combatSort"></i>
+  <i class="fa fa-floppy-o" id="combatSave"></i>
+  <i class="fa fa-spinner" id="combatLoad"></i>
+  <i class="fa fa-pause" id="combatPause"></i>
 </div>
 
 <div class="innerContent">
   <div class="player">
     <img src="" width="80" height="80"/>
-    <div class="stats">
-      Health
-      <i class="fa fa-heart" style="color: #d12020"></i>
-    </div>
-    <div class="stats">
-      Initiative
-      <i class="fa fa-bolt" style="color: #dfdd26"></i>
-    </div>
-    <div class="stats">
-      Spells slots
-      <i class="fa fa-fire" style="color: #e8801b"></i>
-    </div>
-    <div class="stats">
-      Money
-      <i class="fa fa-diamond" style="color: #6fcc23"></i>
-    </div>
-    <div class="stats">
-      Conditions
-      <i class="fa fa-flask" style="color: #"></i>
-    </div>
+    <div class="stats" id="statHealth"><i class="fa fa-heart" style="color: #d12020"></i><input value="50" class="fieldHealth"> | <input value="150" class="fieldHealth"></div>
+    <div class="stats" id="statInitiative"><i class="fa fa-bolt" style="color: #dfdd26"></i><input value="12" class="fieldInitiative"></div>
+    <div class="stats" id="statSlots"><i class="fa fa-fire" style="color: #e8801b"></i><input value="2" class="fieldSlots"></div>
+    <div class="stats" id="statMoney"><i class="fa fa-diamond" style="color: #6fcc23"></i><input value="589" class="fieldMoney"></div>
+    <div class="stats" id="statConditions"><i class="fa fa-flask" style="color: #"></i><input value="blinded" class="fieldCons"></div>
   </div>
 
   <div class="dierollWindow">
