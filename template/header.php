63da95203e1f00d0189b09aa60c04481ba3c833d
Solo244 <solo244@gmail.com>
2016-01-28 15:05:06 +0100

    Convert to Grunt
    Added concat, watch, sass, uglify and livereload via Grunt

--- template/header.php
+++ template/header.php
@@ -6,6 +6,6 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<link rel="shortcut icon" href="images/favicon.ico" />
 		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/global.css">
 	</head>
 	<body>
--- template/includes.php
+++ template/includes.php
@@ -1,7 +1,7 @@
-<script src="js/jquery-2.1.4.min.js"></script>
-<script src="js/jquery-ui-1.10.4.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/libs/jquery-2.1.4.min.js"></script>
+<script src="js/libs/jquery-ui-1.10.4.min.js"></script>
+<script src="js/libs/bootstrap.min.js"></script>
+<script src="js/libs/jquery.mCustomScrollbar.concat.min.js"></script>
 <!-- Templates -->
 <script src="js/template/story.js"></script>
 <script src="js/template/map.js"></script>
