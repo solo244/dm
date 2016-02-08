69479a8eedc3fc7bc976afb513bd2365536f1c57
Solo244 <solo244@gmail.com>
2016-02-08 20:04:47 +0100

    Glorb

--- content/npc/index.php
+++ content/npc/index.php
@@ -17,80 +17,80 @@
 <div class="innerContent" id="allNPCs">
   <div class="goodNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="goodNPCMenu"><?php include 'good\sidebar.php' ?></div>
-    <div class="nNPC" id="gNPC1"><?php include 'good\npc\npc1.php' ?></div>
-    <div class="nNPC" id="gNPC2"><?php include 'good\npc\npc2.php' ?></div>
-    <div class="nNPC" id="gNPC3"><?php include 'good\npc\npc3.php' ?></div>
-    <div class="nNPC" id="gNPC4"><?php include 'good\npc\npc4.php' ?></div>
-    <div class="nNPC" id="gNPC5"><?php include 'good\npc\npc5.php' ?></div>
-    <div class="nNPC" id="gNPC6"><?php include 'good\npc\npc6.php' ?></div>
-    <div class="nNPC" id="gNPC7"><?php include 'good\npc\npc7.php' ?></div>
-    <div class="nNPC" id="gNPC8"><?php include 'good\npc\npc8.php' ?></div>
+    <div class="sidebar scrollPlease" id="goodNPCMenu"><?php include 'good/sidebar.php' ?></div>
+    <div class="nNPC" id="gNPC1"><?php include 'good/npc/npc1.php' ?></div>
+    <div class="nNPC" id="gNPC2"><?php include 'good/npc/npc2.php' ?></div>
+    <div class="nNPC" id="gNPC3"><?php include 'good/npc/npc3.php' ?></div>
+    <div class="nNPC" id="gNPC4"><?php include 'good/npc/npc4.php' ?></div>
+    <div class="nNPC" id="gNPC5"><?php include 'good/npc/npc5.php' ?></div>
+    <div class="nNPC" id="gNPC6"><?php include 'good/npc/npc6.php' ?></div>
+    <div class="nNPC" id="gNPC7"><?php include 'good/npc/npc7.php' ?></div>
+    <div class="nNPC" id="gNPC8"><?php include 'good/npc/npc8.php' ?></div>
   </div>
   <div class="badNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="badNPCMenu"><?php include 'bad\sidebar.php' ?></div>
-    <div class="nNPC" id="bNPC1"><?php include 'bad\npc\npc1.php' ?></div>
-    <div class="nNPC" id="bNPC2"><?php include 'bad\npc\npc2.php' ?></div>
-    <div class="nNPC" id="bNPC3"><?php include 'bad\npc\npc3.php' ?></div>
-    <div class="nNPC" id="bNPC4"><?php include 'bad\npc\npc4.php' ?></div>
-    <div class="nNPC" id="bNPC5"><?php include 'bad\npc\npc5.php' ?></div>
-    <div class="nNPC" id="bNPC6"><?php include 'bad\npc\npc6.php' ?></div>
+    <div class="sidebar scrollPlease" id="badNPCMenu"><?php include 'bad/sidebar.php' ?></div>
+    <div class="nNPC" id="bNPC1"><?php include 'bad/npc/npc1.php' ?></div>
+    <div class="nNPC" id="bNPC2"><?php include 'bad/npc/npc2.php' ?></div>
+    <div class="nNPC" id="bNPC3"><?php include 'bad/npc/npc3.php' ?></div>
+    <div class="nNPC" id="bNPC4"><?php include 'bad/npc/npc4.php' ?></div>
+    <div class="nNPC" id="bNPC5"><?php include 'bad/npc/npc5.php' ?></div>
+    <div class="nNPC" id="bNPC6"><?php include 'bad/npc/npc6.php' ?></div>
   </div>
   <div class="shibukaiNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="shibukaiNPCMenu"><?php include 'shibukai\sidebar.php' ?></div>
-    <div class="nNPC" id="sNPC1"><?php include 'shibukai\npc\npc1.php' ?></div>
-    <div class="nNPC" id="sNPC2"><?php include 'shibukai\npc\npc2.php' ?></div>
-    <div class="nNPC" id="sNPC3"><?php include 'shibukai\npc\npc3.php' ?></div>
-    <div class="nNPC" id="sNPC4"><?php include 'shibukai\npc\npc4.php' ?></div>
-    <div class="nNPC" id="sNPC5"><?php include 'shibukai\npc\npc5.php' ?></div>
-    <div class="nNPC" id="sNPC6"><?php include 'shibukai\npc\npc6.php' ?></div>
-    <div class="nNPC" id="sNPC7"><?php include 'shibukai\npc\npc7.php' ?></div>
+    <div class="sidebar scrollPlease" id="shibukaiNPCMenu"><?php include 'shibukai/sidebar.php' ?></div>
+    <div class="nNPC" id="sNPC1"><?php include 'shibukai/npc/npc1.php' ?></div>
+    <div class="nNPC" id="sNPC2"><?php include 'shibukai/npc/npc2.php' ?></div>
+    <div class="nNPC" id="sNPC3"><?php include 'shibukai/npc/npc3.php' ?></div>
+    <div class="nNPC" id="sNPC4"><?php include 'shibukai/npc/npc4.php' ?></div>
+    <div class="nNPC" id="sNPC5"><?php include 'shibukai/npc/npc5.php' ?></div>
+    <div class="nNPC" id="sNPC6"><?php include 'shibukai/npc/npc6.php' ?></div>
+    <div class="nNPC" id="sNPC7"><?php include 'shibukai/npc/npc7.php' ?></div>
   </div>
   <div class="randomNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="randomNPCMenu"><?php include 'random\sidebar.php' ?></div>
-    <div class="nNPC" id="rNPC1"><?php include 'random\npc\npc1.php' ?></div>
+    <div class="sidebar scrollPlease" id="randomNPCMenu"><?php include 'random/sidebar.php' ?></div>
+    <div class="nNPC" id="rNPC1"><?php include 'random/npc/npc1.php' ?></div>
   </div>
   <div class="groupNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="groupNPCMenu"><?php include 'group\sidebar.php' ?></div>
-    <div class="nNPC" id="grNPC1"><?php include 'group\npc\npc1.php' ?></div>
-    <div class="nNPC" id="grNPC2"><?php include 'group\npc\npc2.php' ?></div>
-    <div class="nNPC" id="grNPC3"><?php include 'group\npc\npc3.php' ?></div>
-    <div class="nNPC" id="grNPC4"><?php include 'group\npc\npc4.php' ?></div>
-    <div class="nNPC" id="grNPC5"><?php include 'group\npc\npc5.php' ?></div>
-    <div class="nNPC" id="grNPC6"><?php include 'group\npc\npc6.php' ?></div>
+    <div class="sidebar scrollPlease" id="groupNPCMenu"><?php include 'group/sidebar.php' ?></div>
+    <div class="nNPC" id="grNPC1"><?php include 'group/npc/npc1.php' ?></div>
+    <div class="nNPC" id="grNPC2"><?php include 'group/npc/npc2.php' ?></div>
+    <div class="nNPC" id="grNPC3"><?php include 'group/npc/npc3.php' ?></div>
+    <div class="nNPC" id="grNPC4"><?php include 'group/npc/npc4.php' ?></div>
+    <div class="nNPC" id="grNPC5"><?php include 'group/npc/npc5.php' ?></div>
+    <div class="nNPC" id="grNPC6"><?php include 'group/npc/npc6.php' ?></div>
   </div>
   <!---->
   <div class="nalaNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="nalaNPCMenu"><?php include 'nala\sidebar.php' ?></div>
-    <div class="nNPC" id="nNPC1"><?php include 'nala\npc\npc1.php' ?></div>
-    <div class="nNPC" id="nNPC2"><?php include 'nala\npc\npc2.php' ?></div>
-    <div class="nNPC" id="nNPC3"><?php include 'nala\npc\npc3.php' ?></div>
-    <div class="nNPC" id="nNPC4"><?php include 'nala\npc\npc4.php' ?></div>
+    <div class="sidebar scrollPlease" id="nalaNPCMenu"><?php include 'nala/sidebar.php' ?></div>
+    <div class="nNPC" id="nNPC1"><?php include 'nala/npc/npc1.php' ?></div>
+    <div class="nNPC" id="nNPC2"><?php include 'nala/npc/npc2.php' ?></div>
+    <div class="nNPC" id="nNPC3"><?php include 'nala/npc/npc3.php' ?></div>
+    <div class="nNPC" id="nNPC4"><?php include 'nala/npc/npc4.php' ?></div>
   </div>
   <div class="laiNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="laiNPCMenu"><?php include 'lai\sidebar.php' ?></div>
-    <div class="nNPC" id="lNPC1"><?php include 'lai\npc\npc1.php' ?></div>
-    <div class="nNPC" id="lNPC2"><?php include 'lai\npc\npc2.php' ?></div>
-    <div class="nNPC" id="lNPC3"><?php include 'lai\npc\npc3.php' ?></div>
-    <div class="nNPC" id="lNPC4"><?php include 'lai\npc\npc4.php' ?></div>
-    <div class="nNPC" id="lNPC5"><?php include 'lai\npc\npc5.php' ?></div>
+    <div class="sidebar scrollPlease" id="laiNPCMenu"><?php include 'lai/sidebar.php' ?></div>
+    <div class="nNPC" id="lNPC1"><?php include 'lai/npc/npc1.php' ?></div>
+    <div class="nNPC" id="lNPC2"><?php include 'lai/npc/npc2.php' ?></div>
+    <div class="nNPC" id="lNPC3"><?php include 'lai/npc/npc3.php' ?></div>
+    <div class="nNPC" id="lNPC4"><?php include 'lai/npc/npc4.php' ?></div>
+    <div class="nNPC" id="lNPC5"><?php include 'lai/npc/npc5.php' ?></div>
   </div>
   <div class="thiaNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="thiaNPCMenu"><?php include 'thia\sidebar.php' ?></div>
-    <div class="nNPC" id="tNPC1"><?php include 'thia\npc\npc1.php' ?></div>
-    <div class="nNPC" id="tNPC2"><?php include 'thia\npc\npc2.php' ?></div>
+    <div class="sidebar scrollPlease" id="thiaNPCMenu"><?php include 'thia/sidebar.php' ?></div>
+    <div class="nNPC" id="tNPC1"><?php include 'thia/npc/npc1.php' ?></div>
+    <div class="nNPC" id="tNPC2"><?php include 'thia/npc/npc2.php' ?></div>
   </div>
   <div class="ottanNPC">
     <i class="fa fa-arrow-left backNPC"></i>
-    <div class="sidebar scrollPlease" id="ottanNPCMenu"><?php include 'ottan\sidebar.php' ?></div>
-    <div class="nNPC" id="oNPC1"><?php include 'ottan\npc\npc1.php' ?></div>
-    <div class="nNPC" id="oNPC2"><?php include 'ottan\npc\npc2.php' ?></div>
+    <div class="sidebar scrollPlease" id="ottanNPCMenu"><?php include 'ottan/sidebar.php' ?></div>
+    <div class="nNPC" id="oNPC1"><?php include 'ottan/npc/npc1.php' ?></div>
+    <div class="nNPC" id="oNPC2"><?php include 'ottan/npc/npc2.php' ?></div>
   </div>
 </div>
